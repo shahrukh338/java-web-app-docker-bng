143f4136d2c2729f15f5b525e70a56ff32834f4f
shahrukh338 <88377687+shahrukh338@users.noreply.github.com>
2022-11-22 22:50:29 +0530

    Update HelloWorldService.java

--- src/main/java/com/rst/helloworld/service/HelloWorldService.java
+++ src/main/java/com/rst/helloworld/service/HelloWorldService.java
@@ -23,7 +23,7 @@ public class HelloWorldService {
 		logger.debug("getTitle() is executed! $name : {}", name);
 
 		if(StringUtils.isEmpty(name)){
-			return "Priya";
+			return "Hinduja Team";
 		}else{
 			return "Hello " + name;
 		}
